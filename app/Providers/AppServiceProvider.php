9d9187f3a30597046bb553cd2133e18348cb1761
Filip <filip.novak@student.mev.hr>
2024-11-25 22:15:30 +0100

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Policies\KnjigePolicy;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Knjige;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       Gate::policy(Knjige::class, KnjigePolicy::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\KnjigeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Middleware\isAdmin;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/users', UsersController::class)->middleware(isAdmin::class);
+    Route::get('knjige', [KnjigeController::class, 'index'])->name('knjige.index');
+    Route::get('knjige/create', [KnjigeController::class, 'create'])->name('knjige.create');
+    Route::post('knjige', [KnjigeController::class, 'store'])->name('knjige.store');
+    Route::get('knjige/{knjige}/edit', [KnjigeController::class, 'edit'])->name('knjige.edit');
+    Route::delete('knjige/{knjige}/destroy', [KnjigeController::class, 'destroy'])->name('knjige.destroy');
+    Route::put('knjige/{knjige}/update', [KnjigeController::class, 'update'])->name('knjige.update');
+    Route::get('auth/home', [App\Http\Controllers\Controller::class, 'index'])->name('auth.home');
+    Route::get('user/home', [App\Http\Controllers\Controller::class, 'index'])->name('user.home');
+   
+});
+
+require __DIR__.'/auth.php';
